56e78a0eed5a763badf0424d31e70ecbf42eaac8
Faisal Sabillah <faisalsabillah2@gmail.com>
2024-12-12 11:48:43 +0700

    git add *

---
+++ html-final-project-library-app-v1/deployData.js
@@ -0,0 +1,15 @@
+/* 
+  buatlah beberapa variable untuk pengecekan deployment
+
+  studentName: berisi string nama student
+  studentId: berisi string CAMPID
+  NetlifyDeployUrl: berisi string url Netlify deployment
+*/
+
+const studentName = 'Faisal Sabillah';
+
+const studentId = 'FE4493962';
+
+const NetlifyDeployUrl = 'https://famous-lebkuchen-52199f.netlify.app'; 
+
+module.exports = { studentName, studentId, NetlifyDeployUrl };
